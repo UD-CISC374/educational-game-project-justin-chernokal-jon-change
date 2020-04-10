667020b8ecb7790f8a7593e2a69cedd65fa24cb4
Justin Chernokal <jcherns@udel.edu>
2020-04-10 13:50:50 -0400

    NPM install + PATH + GRID

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -13,25 +13,48 @@ export default class MainScene extends Phaser.Scene {
   
 
   /* PATH */
+  //graphics used for path visualization: background
   var graphics = this.add.graphics();
   graphics.fillStyle(0x000000, 1);
   graphics.beginPath();
   graphics.moveTo(0, 0);
   graphics.lineTo(this.scale.width, 0);
-  graphics.lineTo(this.scale.width, 400);
-  graphics.lineTo(0, 400);
+  graphics.lineTo(this.scale.width, 512);
+  graphics.lineTo(0, 640);
   graphics.lineTo(0, 0);
   graphics.closePath();
   graphics.fillPath();
 
-  var path = this.add.path(200, -32);
-  path.lineTo(200, 400);
+  //path declaration for enemies
+  var path = this.add.path(352, -32);
+  path.lineTo(352, 512);
 
+  //graphics used for path visualization: line
   graphics.lineStyle(3, 0xffffff, 1);
   path.draw(graphics);
+
+
+  /* GRID */
+  var graphics = this.add.graphics();
+  //drawGrid function is defined below the update function
+  this.drawGrid(graphics);
 
   }
 
   update() {
+  }
+
+
+  drawGrid(graphics){
+    graphics.lineStyle(1, 0x0000ff, 0.8);
+    for(var i = 0; i < 8; i++) {
+      graphics.moveTo(0, i* 64);
+      graphics.lineTo(640, i * 64);
+    }
+    for(var j = 0; j < 10; j++) {
+      graphics.moveTo(j*64, 0);
+      graphics.lineTo(j * 64, 512);
+    }
+    graphics.strokePath();
   }
 }
